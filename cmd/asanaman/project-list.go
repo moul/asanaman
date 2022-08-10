c6707510cce5f5e10885c8ad81686d19503be24d
Manfred Touron <94029+moul@users.noreply.github.com>
2022-08-10 15:31:22 +0000

    feat: add 'project-list --filter-*'
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- cmd/asanaman/project-list.go
+++ cmd/asanaman/project-list.go
@@ -17,12 +17,17 @@ func doProjectList(ctx context.Context, args []string) error {
 
 	g.rootLogger.Debug("project-list", zap.Strings("args", args), zap.Any("g", g))
 
-	opts := asana.ProjectListOpts{}
+	opts := asana.ProjectListOpts{
+		Workspace: g.FilterWorkspace,
+		Team:      g.FilterTeam,
+		Archived:  g.FilterArchived,
+	}
 	ret, err := g.client.ProjectList(ctx, opts)
 	if err != nil {
 		return fmt.Errorf("project-list: %w", err)
 	}
 
 	fmt.Println(u.PrettyJSON(ret))
+	g.rootLogger.Debug("projects", zap.Int("count", len(*ret)))
 	return nil
 }
